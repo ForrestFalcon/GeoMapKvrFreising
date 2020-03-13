a5cb0463d0b91509c181d220bf49d743cce9e13b
Kevin Stieglitz <kevin.stieglitz@jambit.com>
2020-03-13 18:57:09 +0100

    Initial commit

---
+++ data/inspektion.js
@@ -0,0 +1,57 @@
+var fs21 = {
+    name: "Hinterholzer Michael",
+    abschnitt: "2/1"
+};
+var fs22 = {
+    name: "Jasch Reinhold",
+    abschnitt: "2/2"
+};
+var fs31 = {
+    name: "Littel Alexander",
+    abschnitt: "3/1"
+};
+var fs32 = {
+    name: "Frank Erich",
+    abschnitt: "3/2"
+};
+var fs41 = {
+    name: "Neumair Johannes",
+    abschnitt: "4/1"
+};
+var fs42 = {
+    name: "Probst Siegfried",
+    abschnitt: "4/2"
+};
+var fs51 = {
+    name: "Wagensonnder Michael",
+    abschnitt: "5/1"
+};
+var fs52 = {
+    name: "Forster Markus",
+    abschnitt: "5/2"
+};
+
+var kbms = [fs21, fs22, fs31, fs32, fs41, fs42, fs51, fs52];
+
+var fs2 = {
+    name: "Schmid Helmut",
+    abschnitt: "2"
+};
+var fs3 = {
+    name: "Maier Josef",
+    abschnitt: "3"
+};
+var fs4 = {
+    name: "Müller Andreas",
+    abschnitt: "4"
+};
+var fs5 = {
+    name: "Baur Helmut",
+    abschnitt: "5"
+};
+var fs6 = {
+    name: "Frankl Anton",
+    abschnitt: "6"
+};
+
+var kbis = [fs2, fs3, fs4, fs5, fs6];
